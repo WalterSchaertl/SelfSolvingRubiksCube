c35136db99455eb4b553a10d1ceba955d10cae33
Walter Schaertl <wps3581@rit.edu>
2020-09-23 22:53:49 +0000

    Added encoder comments

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -3,6 +3,7 @@ import time
 
 
 def main():
+    # Pocket Beagle Encoder Channel A and B
     ch_a = "P1_8"
     ch_b = "P1_6"
      
@@ -18,14 +19,20 @@ def main():
         a_new = GPIO.input(ch_a)
         b_new = GPIO.input(ch_b)
         
+        # If channel A is a rising edge
         if(a_new == 1 and cha_st == 0):
+            # If channel B is already high, clockwise turn 
             if chb_st == 1:
                 deg += 0.703125
+            # Else, CCW
             else:
                 deg -= 0.703125
  
+        # Update stateas
         cha_st = a_new
         chb_st = b_new
+        
+        # Time spent printing likely misses cycles, so do it infrequently
         if i % 600000 == 0:
             print(deg)
         i += 1
